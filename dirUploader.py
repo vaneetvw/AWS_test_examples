e27d4b82a9049009d0fb7556b7cacc2e317432ed
Mathisco-01 <Mathisco-01@users.noreply.github.com>
2018-03-01 23:15:15 +0100

    Clarification of certain parts

--- dirUploader.py
+++ dirUploader.py
@@ -1,15 +1,24 @@
+# Mathis Van Eetvelde 1st March 2018
+# Automatic directory upload to AWS s3 bucket
+# Where to get access key: https://aws.amazon.com/blogs/security/wheres-my-secret-access-key/
+
+
 import boto3
 import os
 from botocore.client import Config
 
 
-ACCESS_KEY_ID = "-public access key-"
-ACCESS_SECRET_KEY = "-secret access key-"
-BUCKET_NAME = "-bucket name-"
+#CREDENTIALS
+ACCESS_KEY_ID = "public access key"
+ACCESS_SECRET_KEY = "secret access key"
+BUCKET_NAME = "bucket name"
+
+#FILE AND DIR SPECIFICATION
 FILE_NAME=""
-DIR_NAME="-dir you want to upload to specified bucket-"
+DIR_NAME="dir you want to upload to specified bucket ea: C:/Documents/folder"
 iteration = 0
 
+#Connect to s3
 s3 = boto3.resource(
     "s3",
     aws_access_key_id=ACCESS_KEY_ID,
@@ -17,6 +26,7 @@ s3 = boto3.resource(
     config=Config(signature_version="s3v4")
 )
 
+#Cycle trough dir and upload to s3 bucket
 for filename in os.listdir(DIR_NAME):
     print(filename)
     FILE_NAME = (DIR_NAME + '/' + filename)
@@ -25,6 +35,7 @@ for filename in os.listdir(DIR_NAME):
     s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=data)
     FILE_NAME = ""
 
+#listing files in s3 bucket
 listObjSummary = s3.Bucket(BUCKET_NAME).objects.all()
 
 for objSum in listObjSummary:
